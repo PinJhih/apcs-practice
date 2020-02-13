0e21bdc77eb12c6fc38b81103fc95a12373b293e
Kuroki <isaacwang29@gmail.com>
2020-02-13 21:29:15 +0800

    refactor: Change the UI design of the CheckAnswer item

--- app/src/main/java/com/example/apcs_practice/view/adapters/CheckAnswerAdapter.kt
+++ app/src/main/java/com/example/apcs_practice/view/adapters/CheckAnswerAdapter.kt
@@ -14,7 +14,7 @@ import kotlinx.android.synthetic.main.item_check_answer.view.*
 class CheckAnswerAdapter(
     private val context: Context,
     private val myAnswer: String,
-    private val correctAnswer: String
+    private val correctAnswers: String
 ) :
     RecyclerView.Adapter<CheckAnswerAdapter.ViewHolder>() {
     class ViewHolder(v: View) : RecyclerView.ViewHolder(v)
@@ -25,28 +25,28 @@ class CheckAnswerAdapter(
         return ViewHolder(v)
     }
 
-    override fun getItemCount() = correctAnswer.length
+    override fun getItemCount() = correctAnswers.length
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
+        val correctAnswer = "正解: " + correctAnswers[position].toString()
         val itemView = holder.itemView
         val questionNumber = "第${position + 1}題"
-        val bgColor = if (myAnswer[position] == correctAnswer[position]) Color.parseColor("#5EF181")
-        else Color.parseColor("#F75F5F")
+        val bgColor =
+            if (myAnswer[position] == correctAnswers[position]) Color.parseColor("#D5FACE")
+            else Color.parseColor("#FCCACA")
+        val textColor =
+            if (myAnswer[position] == correctAnswers[position]) Color.parseColor("#3C783C")
+            else Color.parseColor("#AA4343")
 
         itemView.tv_question_number.text = questionNumber
-        itemView.tv_correct_answer.text = correctAnswer[position].toString()
+        itemView.tv_correct_answer.text = correctAnswer
+        itemView.tv_question_number.setTextColor(textColor)
+        itemView.tv_correct_answer.setTextColor(textColor)
+        itemView.tv_my_answer.setTextColor(textColor)
         itemView.tv_my_answer.text = myAnswer[position].toString()
         itemView.layout_item_check_answer.setBackgroundColor(bgColor)
         itemView.layout_item_check_answer.setOnClickListener {
             (context as CheckAnswerActivity).reviewQuestion(position)
-        }
-
-        if (settings.getBoolean("darkMode", false)) {
-            val textColor = Color.parseColor("#ffffff")
-
-            itemView.tv_question_number.setTextColor(textColor)
-            itemView.tv_correct_answer.setTextColor(textColor)
-            itemView.tv_my_answer.setTextColor(textColor)
         }
     }
 }
