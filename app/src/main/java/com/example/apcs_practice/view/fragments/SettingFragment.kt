8da2eeadf27174b6ddc6a57ef63f9e0f6ae6134f
Kuroki <isaacwang29@gmail.com>
2020-02-20 15:55:42 +0800

    feat: Delete all the histories and reset settings in setting page

--- app/src/main/java/com/example/apcs_practice/view/fragments/SettingFragment.kt
+++ app/src/main/java/com/example/apcs_practice/view/fragments/SettingFragment.kt
@@ -1,12 +1,15 @@
 package com.example.apcs_practice.view.fragments
 
+import android.app.AlertDialog
 import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.apcs_practice.R
+import com.example.apcs_practice.database.HistoryDBHelper
 import com.example.apcs_practice.view.activities.settings
 import kotlinx.android.synthetic.main.fragment_setting.*
 
@@ -23,11 +26,13 @@ class SettingFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        /*
         switch_dark_mode.setOnClickListener {
             settingEditor.putBoolean("darkMode", switch_dark_mode.isChecked)
             settingEditor.commit()
             setView()
         }
+        */
         switch_notification_test.setOnClickListener {
             settingEditor.putBoolean("notificationTest", switch_notification_test.isChecked)
             settingEditor.commit()
@@ -44,20 +49,49 @@ class SettingFragment : Fragment() {
             settingEditor.putBoolean("queryResultsStart", switch_query_results.isChecked)
             settingEditor.commit()
         }
+        tv_delete_histories.setOnClickListener {
+            AlertDialog.Builder(context)
+                .setTitle("刪除所有歷史紀錄")
+                .setNegativeButton("取消") { _, _ -> }
+                .setPositiveButton("確定") { _, _ ->
+                    val db = HistoryDBHelper(context!!).writableDatabase
+                    val histories = db.rawQuery("SELECT * FROM histories", null)
+                    if (histories.count != 0) {
+                        db.execSQL("DELETE FROM histories")
+                        Toast.makeText(context, "已刪除${histories.count}筆資料", Toast.LENGTH_SHORT)
+                            .show()
+                    } else
+                        Toast.makeText(context, "沒有歷史紀錄", Toast.LENGTH_SHORT).show()
+                    histories.close()
+                    db.close()
+                }
+        }
+        tv_reset_settings.setOnClickListener {
+            settingEditor.putBoolean("notificationTest", true)
+            settingEditor.putBoolean("notificationSignUpStart", true)
+            settingEditor.putBoolean("notificationSignUpEnd", true)
+            settingEditor.putBoolean("queryResultsStart", true)
+            settingEditor.putString("textSize", "mid")
 
-        init()
+            settingEditor.commit()
+            initSwitches()
+        }
+
+        initSwitches()
     }
 
-    private fun init(){
+    private fun initSwitches() {
+        /*
         if (settings.getBoolean("darkMode", false))
             switch_dark_mode.isChecked = true
-        if (settings.getBoolean("notificationTest", false))
+        */
+        if (settings.getBoolean("notificationTest", true))
             switch_notification_test.isChecked = true
-        if (settings.getBoolean("notificationSignUpStart", false))
+        if (settings.getBoolean("notificationSignUpStart", true))
             switch_sign_up_start.isChecked = true
-        if (settings.getBoolean("notificationSignUpEnd", false))
+        if (settings.getBoolean("notificationSignUpEnd", true))
             switch_sign_up_end.isChecked = true
-        if (settings.getBoolean("queryResultsStart", false))
+        if (settings.getBoolean("queryResultsStart", true))
             switch_query_results.isChecked = true
 
         setView()
@@ -71,7 +105,9 @@ class SettingFragment : Fragment() {
             textColor = Color.parseColor("#FFFFFF")
         }
         layout_setting.setBackgroundColor(backgroundColor)
+        /*
         switch_dark_mode.setTextColor(textColor)
+        */
         switch_notification_test.setTextColor(textColor)
         switch_sign_up_start.setTextColor(textColor)
         switch_sign_up_end.setTextColor(textColor)
