70acb74818378bd4d51e0bf2e9004aa4ec714602
PinJhih <isaacwang29@gmail.com>
2020-02-21 20:26:48 +0800

    feat: Send notification only at 8 a.m.

--- app/src/main/java/com/example/apcs_practice/services/NotificationService.kt
+++ app/src/main/java/com/example/apcs_practice/services/NotificationService.kt
@@ -15,68 +15,89 @@ import java.util.*
 
 class NotificationService : Service() {
 
+    private var dates = TestInfo()
+
     override fun onCreate() {
         super.onCreate()
 
         val fireStore = FirebaseFirestore.getInstance()
         fireStore.collection("test_info").document("dates")
             .addSnapshotListener { querySnapshot, _ ->
-                val dates = querySnapshot!!.toObject(TestInfo::class.java)!!
-                sendNotification(dates)
+                dates = querySnapshot!!.toObject(TestInfo::class.java)!!
             }
+
+        sendNotification()
     }
 
-    private fun sendNotification(dates: TestInfo) {
+    private fun sendNotification() {
         val channel = NotificationChannel("Ch1", "APCS", NotificationManager.IMPORTANCE_HIGH)
         val manager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
         manager.createNotificationChannel(channel)
 
-        val cal = Calendar.getInstance()
-        cal.get(Calendar.YEAR)
-        cal.get(Calendar.MONTH)
-        cal.get(Calendar.DAY_OF_MONTH)
-        val myFormat = "MM/dd"
-        val sdf = SimpleDateFormat(myFormat, Locale.TAIWAN)
-        val date = sdf.format(cal.time)
+        Thread(Runnable {
+            while (true) {
+                val cal = Calendar.getInstance()
+                cal.get(Calendar.YEAR)
+                cal.get(Calendar.MONTH)
+                cal.get(Calendar.DAY_OF_MONTH)
+                val myFormat = "MM/dd"
+                val sdf = SimpleDateFormat(myFormat, Locale.TAIWAN)
+                val date = sdf.format(cal.time)
+                val time = settings.getString("notificationTime", "/08")
 
-        if (settings.getBoolean("notificationTest", true) && date == dates.test) {
-            val notification = Notification.Builder(this, "Ch1")
-                .setSmallIcon(R.drawable.notification_icon_background)
-                .setContentTitle("APCS檢測日")
-                .setContentText("今天是APCS檢測日，請注意考試時間")
-                .setAutoCancel(true)
-                .build()
-            manager.notify(0, notification)
-        }
+                if (settings.getBoolean("notificationTest", true) && date == dates.test + time) {
+                    val notification = Notification.Builder(this, "Ch1")
+                        .setSmallIcon(R.drawable.notification_icon_background)
+                        .setContentTitle("APCS檢測日")
+                        .setContentText("今天是APCS檢測日，請注意考試時間")
+                        .setAutoCancel(true)
+                        .build()
+                    manager.notify(0, notification)
+                }
 
-        if (settings.getBoolean("notificationSignUpStart", true) && date == dates.signUpStart) {
-            val notification = Notification.Builder(this, "Ch1")
-                .setSmallIcon(R.drawable.notification_icon_background)
-                .setContentTitle("APCS開始報名")
-                .setContentText("APCS${dates.test}場次開始報名，報名時間${dates.signUpStart}~${dates.signUpDeadline}")
-                .setAutoCancel(true)
-                .build()
-            manager.notify(1, notification)
-        }
+                if (settings.getBoolean(
+                        "notificationSignUpStart",
+                        true
+                    ) && date == dates.signUpStart + time
+                ) {
+                    val notification = Notification.Builder(this, "Ch1")
+                        .setSmallIcon(R.drawable.notification_icon_background)
+                        .setContentTitle("APCS開始報名")
+                        .setContentText("APCS${dates.test}場次開始報名，報名時間${dates.signUpStart}~${dates.signUpDeadline}")
+                        .setAutoCancel(true)
+                        .build()
+                    manager.notify(1, notification)
+                }
 
-        if (settings.getBoolean("notificationSignUpEnd", true) && date == dates.signUpDeadline) {
-            val notification = Notification.Builder(this, "Ch1")
-                .setSmallIcon(R.drawable.notification_icon_background)
-                .setContentTitle("APCS報名最後一天")
-                .setContentText("今天是APCS${dates.test}場次報名最後一天，記得要報名喔")
-                .setAutoCancel(true)
-                .build()
-            manager.notify(2, notification)
-        }
-        if (settings.getBoolean("queryResultsStart", true) && date == dates.queryResults) {
-            val notification = Notification.Builder(this, "Ch1")
-                .setSmallIcon(R.drawable.notification_icon_background)
-                .setContentTitle("成績查詢開始")
-                .setContentText("APCS${dates.test}場次開放查詢")
-                .setAutoCancel(true)
-                .build()
-            manager.notify(3, notification)
-        }
+                if (settings.getBoolean(
+                        "notificationSignUpEnd",
+                        true
+                    ) && date == dates.signUpDeadline + time
+                ) {
+                    val notification = Notification.Builder(this, "Ch1")
+                        .setSmallIcon(R.drawable.notification_icon_background)
+                        .setContentTitle("APCS報名最後一天")
+                        .setContentText("今天是APCS${dates.test}場次報名最後一天，記得要報名喔")
+                        .setAutoCancel(true)
+                        .build()
+                    manager.notify(2, notification)
+                }
+                if (settings.getBoolean(
+                        "queryResultsStart",
+                        true
+                    ) && date == dates.queryResults + time
+                ) {
+                    val notification = Notification.Builder(this, "Ch1")
+                        .setSmallIcon(R.drawable.notification_icon_background)
+                        .setContentTitle("成績查詢開始")
+                        .setContentText("APCS${dates.test}場次開放查詢")
+                        .setAutoCancel(true)
+                        .build()
+                    manager.notify(3, notification)
+                }
+                Thread.sleep(300000)
+            }
+        }).start()
     }
 
     override fun onBind(intent: Intent): IBinder? {
