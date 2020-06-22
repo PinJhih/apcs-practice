53a66ca9e73229845e5c03bd558360b2f47cb167
PinJhih <isaacwang29@gmail.com>
2020-06-22 18:30:51 +0800

    refactor: 移除不需要的 class

--- app/src/main/java/com/example/apcs_practice/models/Detailed.kt
+++
@@ -1,6 +0,0 @@
-package com.example.apcs_practice.models
-
-data class Detailed(
-    var id: String = "",
-    var content: String = ""
-)
--- app/src/main/java/com/example/apcs_practice/models/TestInfo.kt
+++
@@ -1,8 +0,0 @@
-package com.example.apcs_practice.models
-
-data class TestInfo(
-    var test: String = "",
-    var signUpStart: String = "",
-    var signUpDeadline: String = "",
-    var queryResults: String = ""
-)
